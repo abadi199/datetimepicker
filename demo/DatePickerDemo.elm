0a2d3b959b1eabd2dd80165a2f410b9cc9cbe8b3
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2016-12-28 00:54:56 -0600

    Add demo app.

---
+++ demo/DatePickerDemo.elm
@@ -0,0 +1,73 @@
+module DatePickerDemo exposing (main)
+
+import Html exposing (Html, text, p, label, form, ul, li, div)
+import DateTimePicker
+import DateTimePicker.Config exposing (defaultDatePickerConfig, defaultDateTimePickerConfig)
+import Date exposing (Date)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { selectedDate : Maybe Date
+    , datePickerState : DateTimePicker.State
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { selectedDate = Nothing
+      , datePickerState = DateTimePicker.initialState
+      }
+    , Cmd.batch
+        [ DateTimePicker.initialCmd DateChange DateTimePicker.initialState
+        ]
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    form []
+        [ p
+            []
+            [ label []
+                [ text "Date Picker: "
+                , DateTimePicker.datePicker
+                    DateChange
+                    []
+                    model.datePickerState
+                    model.selectedDate
+                ]
+            ]
+        , p []
+            [ text <| "Selected Date: " ++ (Maybe.withDefault "Nothing" <| Maybe.map toString model.selectedDate)
+            ]
+        ]
+
+
+type Msg
+    = NoOp
+    | DateChange DateTimePicker.State (Maybe Date)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        DateChange state value ->
+            ( { model | selectedDate = value, datePickerState = state }, Cmd.none )
---
+++ demo/DigitalDateTimePickerDemo.elm
@@ -0,0 +1,80 @@
+module DigitalDateTimePickerDemo exposing (main)
+
+import Html exposing (Html, text, p, label, form, ul, li, div)
+import DateTimePicker
+import DateTimePicker.Config exposing (defaultDatePickerConfig, defaultDateTimePickerConfig)
+import Date exposing (Date)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { selectedDate : Maybe Date
+    , datePickerState : DateTimePicker.State
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { selectedDate = Nothing
+      , datePickerState = DateTimePicker.initialState
+      }
+    , Cmd.batch
+        [ DateTimePicker.initialCmd DateChange DateTimePicker.initialState
+        ]
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    let
+        default =
+            DateTimePicker.Config.defaultDateTimePickerConfig DateChange
+
+        config =
+            { default | timePickerType = DateTimePicker.Config.Digital }
+    in
+        form []
+            [ p
+                []
+                [ label []
+                    [ text "Digital Date Time Picker: "
+                    , DateTimePicker.dateTimePickerWithConfig
+                        config
+                        []
+                        model.datePickerState
+                        model.selectedDate
+                    ]
+                ]
+            , p []
+                [ text <| "Selected Date and Time: " ++ (Maybe.withDefault "Nothing" <| Maybe.map toString model.selectedDate)
+                ]
+            ]
+
+
+type Msg
+    = NoOp
+    | DateChange DateTimePicker.State (Maybe Date)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        DateChange state value ->
+            ( { model | selectedDate = value, datePickerState = state }, Cmd.none )
